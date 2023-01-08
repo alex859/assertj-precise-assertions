0dd12a7167f9f686dab7ae2cfdecf7f7bbc48018
Alessandro Ciccimarra <alessandro.ciccimarra@gmail.com>
2023-01-08 14:49:30 +0000

    add test for condition success and failure

--- src/test/java/org/assertj/core/condition/CollectionConditionsTest.java
+++ src/test/java/org/assertj/core/condition/CollectionConditionsTest.java
@@ -2,7 +2,6 @@ package org.assertj.core.condition;
 
 import domain.Address;
 import domain.House;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,12 +10,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.condition.CollectionsConditions.contains;
 import static org.assertj.core.condition.CollectionsConditions.elementAt;
-import static org.assertj.core.condition.Conditions.*;
+import static org.assertj.core.condition.Conditions.area;
+import static org.assertj.core.condition.Conditions.house;
+import static org.assertj.core.condition.Conditions.houses;
+import static org.assertj.core.condition.Conditions.wallType;
 
 class CollectionConditionsTest {
     @Test
-    @DisplayName("contains failure message")
-    void containsHouse() {
+    void containsThrows() {
+        var house1 = new House(12, "bricks");
+        var house2 = new House(1, "bricks");
+        var address = new Address("line 1", "line 2", "E31QT", List.of(house1, house2));
+
+        assertThatThrownBy(() -> assertThat(address).has(
+                houses(
+                        contains(house(area(2), wallType("bricks"))),
+                        contains(house(area(1), wallType("bricks")))
+                )
+
+        ));
+    }
+
+    @Test
+    void containsSucceeds() {
+        var house1 = new House(12, "bricks");
+        var house2 = new House(1, "cement");
+        var address = new Address("line 1", "line 2", "E31QT", List.of(house1, house2));
+
+        assertThat(address).has(
+                houses(
+                        contains(house(area(1), wallType("cement"))),
+                        contains(house(area(12), wallType("bricks")))
+                )
+
+        );
+    }
+
+    @Test
+    void containsMessage() {
         var house1 = new House(12, "bricks");
         var house2 = new House(1, "bricks");
         var address = new Address("line 1", "line 2", "E31QT", List.of(house1, house2));
@@ -44,10 +75,38 @@ class CollectionConditionsTest {
                 ]""".stripIndent());
     }
 
+    @Test
+    void elementAtFails() {
+        var house1 = new House(12, "bricks");
+        var house2 = new House(1, "bricks");
+        var address = new Address("line 1", "line 2", "E31QT", List.of(house1, house2));
+
+        assertThatThrownBy(() -> assertThat(address).has(
+                houses(
+                        elementAt(0, house(area(2), wallType("bricks"))),
+                        elementAt(1, house(area(1), wallType("bricks")))
+                )
+
+        ));
+    }
 
     @Test
-    @DisplayName("element at failure message")
-    void containsHouseAtPosition() {
+    void elementAtSuccedes() {
+        var house1 = new House(12, "bricks");
+        var house2 = new House(1, "bricks");
+        var address = new Address("line 1", "line 2", "E31QT", List.of(house1, house2));
+
+        assertThat(address).has(
+                houses(
+                        elementAt(0, house(area(12), wallType("bricks"))),
+                        elementAt(1, house(area(1), wallType("bricks")))
+                )
+
+        );
+    }
+    
+    @Test
+    void elementAtMessage() {
         var house1 = new House(12, "bricks");
         var house2 = new House(1, "bricks");
         var address = new Address("line 1", "line 2", "E31QT", List.of(house1, house2));
